81f4786668ad3bdf3705a87bcb36b212e8dd0a58
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-23 19:36:19 +0530

    Fibonacci Series Added

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public static void GetFibonacciSeries()
+        {
+            int num1 = 0, num2 = 1, num3;
+            Console.WriteLine("Enter a number : ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.Write(num1 + "," + num2);
+            for (int i = 2; i <= count;i++)
+            {
+                num3 = num1 + num2;
+                Console.Write(","+num3);
+                num1 = num2;
+                num2 = num3;        
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace LogicalPrograms
+﻿using System.Drawing;
+
+namespace LogicalPrograms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs!");
+            Console.WriteLine("\n1. Fibonacci Series.");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                // Q1. Fibonacci Series
+                case 1:
+                    FibonacciSeries.GetFibonacciSeries();
+                    break;
+                // Out of range question
+                default:
+                    Console.WriteLine("Choose given option only.");
+                    break;
+            }
         }
     }
 }
