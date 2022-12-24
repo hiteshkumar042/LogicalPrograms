e8289cfed755421e74025476957916bb8c36ec0a
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-25 01:02:52 +0530

    DecimalToBinary Program Added

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs!");
-            Console.WriteLine("\n1. Fibonacci Series. \n2. Perfect Number. \n3. Prime Number. \n4. Reverse Number. \n5. Coupon Numbers. \n6. StopWatch.");
-            Console.WriteLine("7. Vending Machine.\n8. Day of Week.\n9. Temperature Conversion.\n10. Montly Payment.");
-            Console.WriteLine("11.Square Root\n");
+            Console.WriteLine("\n01. Fibonacci Series. \n02. Perfect Number. \n03. Prime Number. \n04. Reverse Number. \n05. Coupon Numbers. \n06. StopWatch.");
+            Console.WriteLine("07. Vending Machine.\n08. Day of Week.\n09. Temperature Conversion.\n10. Montly Payment.");
+            Console.WriteLine("11. Square Root. \n12. Decimal to Binary.\n");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -55,11 +55,14 @@
                 case 11:
                     SquareRoot.GetSquareRoot();
                     break;
+                //Q 12. Binary to Decimal
+                case 12:
+                    ToBinary.GetBinary();
+                    break;
                 // Out of range question
                 default:
                     Console.WriteLine("Choose given option only.");
                     break;
-                
             }
         }
         
---
+++ LogicalPrograms/ToBinary.cs
@@ -0,0 +1,22 @@
+﻿namespace LogicalPrograms
+{
+    internal class ToBinary
+    {
+        public static int[] binary = new int[8];
+        public static void GetBinary()
+        {
+            Console.WriteLine("Enter a Decimal Number : ");
+            int decNum = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; decNum > 0; i++)
+            {
+                binary[i] = decNum % 2;
+                decNum /= 2;
+                Console.Write(binary[i] + " ");
+            }
+            for (int i = binary.Length-1; i >= 0; i--)
+            {
+                Console.Write("\n"+binary[i] + " ");
+            }
+        }
+    }
+}
